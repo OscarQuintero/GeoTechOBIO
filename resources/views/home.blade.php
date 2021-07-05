5bb02edf3c55bfaea9011a68678cedb9f2e6ec25
Oscar Quintero <oscar24quintero@hotmail.com>
2021-07-05 01:26:42 -0500

    Cambios en los layouts y adicion de Procfile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.basic')
 @section('content')
+<h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200"
+            >Catálogo</h2>
+
 En esta parte aparecerá el catalgo de los productos
 <br>
 
