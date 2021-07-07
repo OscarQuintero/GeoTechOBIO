ce82c72f5e207b60a71fafa75311365f4d320114
Oscar Quintero <oscar24quintero@hotmail.com>
2021-07-07 16:49:55 -0500

    Imagenes en los items del catalogo y mejora de la vista de detalle de producto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -38,13 +38,8 @@
               <a href="/product-detail/Café Prodyser">
               <div class="flex items-center p-4 rounded-lg shadow-xs dark:bg-gray-800"
               style="background-color: #bd9a5f;">
-              <img src="{{ asset('img/prod.png')}}" width="80px">
-                <div class="p-3 mr-4 text-orange-500 rounded-full dark:text-orange-100 dark:bg-orange-500"
-                style="background-color: #323e48; color: white;">
-                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7"/>
-                  </svg>
-                </div>
+              <img src="{{ asset('img/prod.png')}}" width="80px" class="rounded-full">
+                
                 <div>
                   <p class="mb-2 text-sm font-medium text-white-600 dark:text-gray-400">
                     Café Prodyser
@@ -63,15 +58,10 @@
               <a href="/product-detail/Café la organización">
               <div class="flex items-center p-4 rounded-lg shadow-xs dark:bg-gray-800"
               style="background-color: #757b63;">
-                <div class="p-3 mr-4 text-green-500 rounded-full dark:text-green-100 dark:bg-green-500"
-                style="background-color: #323e48; color: white;">
-                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" />
-                  </svg>
-                </div>
+                <img src="{{ asset('img/prod.png')}}" width="80px" class="rounded-full">
                 <div>
                   <p class="mb-2 text-sm font-medium text-white-600 dark:text-gray-400">
-                    Café la organización
+                    Café la organi...
                   </p>
                   <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
                     $ 46.78
@@ -85,12 +75,7 @@
               <!-- Card -->
                 <a href="/product-detail/Café MAM">
               <div class="flex items-center p-4 rounded-lg shadow-xs dark:bg-gray-800" style="background-color: #fcaf1a;">
-                <div class="p-3 mr-4 text-blue-500 rounded-full dark:text-blue-100 dark:bg-blue-500"
-                style="background-color: #323e48; color: white;">
-                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" />
-                  </svg>
-                </div>
+                <img src="{{ asset('img/prod.png')}}" width="80px" class="rounded-full">
                 <div>
                   <p class="mb-2 text-sm font-medium text-white-600 dark:text-gray-400">
                     Café MAM
@@ -108,13 +93,7 @@
                 <a href="/product-detail/Café UICIRI">
               <div class="flex items-center p-4 rounded-lg shadow-xs dark:bg-gray-800"
               style="background-color: #ff6f4a;">
-                <div class="p-3 mr-4 text-teal-500 rounded-full dark:text-teal-100 dark:bg-teal-500"
-                style="background-color: #323e48; color: white;">
-                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" />
-                  </svg>
-
-                </div>
+                <img src="{{ asset('img/prod.png')}}" width="80px" class="rounded-full">
                 <div>
                   <p class="mb-2 text-sm font-medium text-white-600 dark:text-gray-400">
                     Café UICIRI
@@ -137,12 +116,7 @@
               <a href="/product-detail/Café Prodyser">
               <div class="flex items-center p-4 rounded-lg shadow-xs dark:bg-gray-800"
               style="background-color: #bd9a5f;">
-                <div class="p-3 mr-4 text-orange-500 rounded-full dark:text-orange-100 dark:bg-orange-500"
-                style="background-color: #323e48; color: white;">
-                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" />
-                  </svg>
-                </div>
+                <img src="{{ asset('img/prod.png')}}" width="80px" class="rounded-full">
                 <div>
                   <p class="mb-2 text-sm font-medium text-white-600 dark:text-gray-400">
                     Café Prodyser
@@ -160,12 +134,7 @@
                 <a href="/product-detail/Café la organización">
               <div class="flex items-center p-4 rounded-lg shadow-xs dark:bg-gray-800"
               style="background-color: #757b63;">
-                <div class="p-3 mr-4 text-green-500 rounded-full dark:text-green-100 dark:bg-green-500"
-                style="background-color: #323e48; color: white;">
-                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" />
-                  </svg>
-                </div>
+                <img src="{{ asset('img/prod.png')}}" width="80px" class="rounded-full">
                 <div>
                   <p class="mb-2 text-sm font-medium text-white-600 dark:text-gray-400">
                     Café la organización
@@ -182,12 +151,7 @@
               <!-- Card -->
               <a href="/product-detail/Café MAM">
               <div class="flex items-center p-4 rounded-lg shadow-xs dark:bg-gray-800" style="background-color: #fcaf1a;">
-                <div class="p-3 mr-4 text-blue-500 rounded-full dark:text-blue-100 dark:bg-blue-500"
-                style="background-color: #323e48; color: white;">
-                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" />
-                  </svg>
-                </div>
+                <img src="{{ asset('img/prod.png')}}" width="80px" class="rounded-full">
                 <div>
                   <p class="mb-2 text-sm font-medium text-white-600 dark:text-gray-400">
                     Café MAM
@@ -205,13 +169,7 @@
               <a href="/product-detail/Café UICIRI">
               <div class="flex items-center p-4 rounded-lg shadow-xs dark:bg-gray-800"
               style="background-color: #ff6f4a;">
-                <div class="p-3 mr-4 text-teal-500 rounded-full dark:text-teal-100 dark:bg-teal-500"
-                style="background-color: #323e48; color: white;">
-                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" />
-                  </svg>
-
-                </div>
+                <img src="{{ asset('img/prod.png')}}" width="80px" class="rounded-full">
                 <div>
                   <p class="mb-2 text-sm font-medium text-white-600 dark:text-gray-400">
                     Café UICIRI
@@ -228,12 +186,7 @@
 
               <a href="/product-detail/Café MAM">
               <div class="flex items-center p-4 rounded-lg shadow-xs dark:bg-gray-800" style="background-color: #f58234;">
-                <div class="p-3 mr-4 text-blue-500 rounded-full dark:text-blue-100 dark:bg-blue-500"
-                style="background-color: #323e48; color: white;">
-                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" />
-                  </svg>
-                </div>
+                <img src="{{ asset('img/prod.png')}}" width="80px" class="rounded-full">
                 <div>
                   <p class="mb-2 text-sm font-medium text-white-600 dark:text-gray-400">
                     Café MAM
@@ -252,13 +205,7 @@
               <a href="/product-detail/Café UICIRI">
               <div class="flex items-center p-4 rounded-lg shadow-xs dark:bg-gray-800"
               style="background-color: #757b63;">
-                <div class="p-3 mr-4 text-teal-500 rounded-full dark:text-teal-100 dark:bg-teal-500"
-                style="background-color: #323e48; color: white;">
-                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" />
-                  </svg>
-
-                </div>
+                <img src="{{ asset('img/prod.png')}}" width="80px" class="rounded-full">
                 <div>
                   <p class="mb-2 text-sm font-medium text-white-600 dark:text-gray-400">
                     Café UICIRI
--- resources/views/product-detail.blade.php
+++ resources/views/product-detail.blade.php
@@ -2,14 +2,24 @@
 @section('content')
 	<h2 class="my-6 text-2xl font-semibold text-main-color-obio-marron dark:text-main-color-obio-amarillo">{{ $producto }}</h2>
 
-            <img src="{{ asset('img/prod.png')}}">
+        	<img src="{{ asset('img/prod.png')}}">
 
-            <p>
-            	 Descripción del producto
-           	</p>
-           	<br>
-           	<h3>$46.56</h3>
-            <br/>
+        
+        	<div class="grid gap-6 mb-8 md:grid-cols-2">           
+
+	       	 	<div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+		            <h4 class="mb-4 font-semibold text-gray-600 dark:text-gray-300">
+		              $46.56
+		            </h4>
+		            <p class="text-gray-600 dark:text-gray-400">
+		              Café Tostado y Molido La Organización, café de altura, café de Oaxaca.
+						Café de aroma florar, de avellanas y fruta seca, acidez muy fina, cuerpo redondo y cremoso, sabor dulce acaramelado.
+						Los granos se cultivan en un rango de altitud de 900 a 1700 metros, en cuatro regiones Oaxaca: Mixteca, Sierra Sur, Costa y Sierra Norte; por 4500 pequeños productores de café sustentable orgánico y de comercio justo, integrados en la Coordinadora Estatal de Productores de Café de Oaxaca (CEPCO), creada para conquistar un mercado nacional que demanda café de alta calidad.
+		            </p>
+		        </div>
+	        </div>
+
+        
             <div class="px-6 my-6">
             <button
               class="flex items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
