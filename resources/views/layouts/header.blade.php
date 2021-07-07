85a0a3dbdb39509ad1b02c44e677f2855d91e192
Oscar Quintero <oscar24quintero@hotmail.com>
2021-07-07 15:34:50 -0500

    Icono del desplegable del modo

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -96,13 +96,10 @@
                   aria-haspopup="true"
                 >
                   <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                   </svg>
-                  <!-- Notification badge -->
-                  <span
-                    aria-hidden="true"
-                    class="absolute top-0 right-0 inline-block w-3 h-3 transform translate-x-1 -translate-y-1 bg-red-600 border-2 border-white rounded-full dark:border-gray-800"
-                  >5</span>
+
+                  
                 </button>
                 <template x-if="isModeMenuOpen">
                   <ul
@@ -117,7 +114,7 @@
                     <li class="flex">
                       <a
                         class="inline-flex items-center justify-between w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
-                        href="/cart"
+                        href="#"
                       >
                         <span>Menudeo</span>
                         <span
@@ -130,7 +127,7 @@
                     <li class="flex">
                       <a
                         class="inline-flex items-center justify-between w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
-                        href="/cart"
+                        href="#"
                       >
                         <span>Medio mayoreo</span>
                         <span
@@ -143,7 +140,7 @@
                     <li class="flex">
                       <a
                         class="inline-flex items-center justify-between w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
-                        href="/cart"
+                        href="#"
                       >
                         <span>Mayoreo</span>
                         <span
