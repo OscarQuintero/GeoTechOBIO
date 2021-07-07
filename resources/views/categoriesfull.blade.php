219da237846d177210c5e3b086a9e862a0371186
Oscar Quintero <oscar24quintero@hotmail.com>
2021-07-07 14:12:21 -0500

    Adicion de links a la lista completa de categorias

--- resources/views/categoriesfull.blade.php
+++ resources/views/categoriesfull.blade.php
@@ -4,54 +4,54 @@
     >Categorías</h2>
 <ul>
     <li>
-        <h3 class="my-6 text-2xl font-semibold text-main-color-obio-chedron dark:text-main-color-obio-amarillo">-Café</h3>
+        <h3 class="my-6 text-2xl font-semibold text-main-color-obio-chedron dark:text-main-color-obio-amarillo"><a href="/catalogo/Café">Café</a></h3>
         <ul>
-            <li>Café Orgánico Tostado y Molido</li>
-            <li>Café Orgánico Gourmet Tostado y Molido</li>
-            <li>Café Orgánico en Grano Tostado</li>
-            <li>Café Liofilizado</li>
-            <li>Café orgánico Soluble</li>
+            <li><a href="/catalogo/Café Orgánico Tostado y Molido">Café Orgánico Tostado y Molido</a></li>
+            <li><a href="/catalogo/Café Orgánico Gourmet Tostado y Molido">Café Orgánico Gourmet Tostado y Molido</a></li>
+            <li><a href="/catalogo/Café Orgánico en Grano Tostado">Café Orgánico en Grano Tostado</a></li>
+            <li><a href="/catalogo/Café Liofilizado">Café Liofilizado</a></li>
+            <li><a href="/catalogo/Café orgánico Soluble">Café orgánico Soluble</a></li>
         </ul>
     </li>
     <li>
-        <h3 class="my-6 text-2xl font-semibold text-main-color-obio-chedron dark:text-main-color-obio-amarillo"class="my-6 text-2xl font-semibold text-main-color-obio-chedron dark:text-main-color-obio-amarillo">-De Cacao</h3>
+        <h3 class="my-6 text-2xl font-semibold text-main-color-obio-chedron dark:text-main-color-obio-amarillo"class="my-6 text-2xl font-semibold text-main-color-obio-chedron dark:text-main-color-obio-amarillo"><a href="/catalogo/Cacao">De Cacao</a></h3>
         <ul>
-            <li>Chocolate Miel con Cacao untable</li>
-            <li>Nibs de Cacao</li>
-            <li>Barra de Licor de Cacao</li>
-            <li>Barra de Chocolate Oscuro</li>
-            <li>Barra de Chocolate de Mesa</li>
+            <li><a href="/catalogo/Chocolate Miel con Cacao untable">Chocolate Miel con Cacao untable</a></li>
+            <li><a href="/catalogo/Nibs de Cacao">Nibs de Cacao</a></li>
+            <li><a href="/catalogo/Barra de Licor de Cacao">Barra de Licor de Cacao</a></li>
+            <li><a href="/catalogo/Barra de Chocolate Oscuro">Barra de Chocolate Oscuro</a></li>
+            <li><a href="/catalogo/Barra de Chocolate de Mesa">Barra de Chocolate de Mesa</a></li>
         </ul>
     </li>
     <li>
-        <h3 class="my-6 text-2xl font-semibold text-main-color-obio-chedron dark:text-main-color-obio-amarillo">-Miel de Abeja</h3>
+        <h3 class="my-6 text-2xl font-semibold text-main-color-obio-chedron dark:text-main-color-obio-amarillo"><a href="/catalogo/Miel de Abeja">Miel de Abeja</a></h3>
         <ul>
-            <li>Miel Diferenciada por Floración</li>
-            <li>Miel Multiflora Orgánica</li>
-            <li>Miel Multiflora Mantequilla</li>
-            <li>Miel de abeja Melipona</li>
+            <li><a href="/catalogo/Miel Diferenciada por Floración">Miel Diferenciada por Floración</a></li>
+            <li><a href="/catalogo/Miel Multiflora Orgánica">Miel Multiflora Orgánica</a></li>
+            <li><a href="/catalogo/Miel Multiflora Mantequilla">Miel Multiflora Mantequilla</a></li>
+            <li><a href="/catalogo/Miel de abeja Melipona">Miel de abeja Melipona</a></li>
             
         </ul>
     </li>
     <li>
-        <h3 class="my-6 text-2xl font-semibold text-main-color-obio-chedron dark:text-main-color-obio-amarillo">-Artesanía</h3>
+        <h3 class="my-6 text-2xl font-semibold text-main-color-obio-chedron dark:text-main-color-obio-amarillo"><a href="/catalogo/Artesanía">Artesanía</a></h3>
         <ul>
-            <li>De madera</li>
-            <li>De hoja de pino</li>
+            <li><a href="/catalogo/De madera">De madera</a></li>
+            <li><a href="/catalogo/De hoja de pino">De hoja de pino</a></li>
             
         </ul>
     </li>
     <li>
-        <h3 class="my-6 text-2xl font-semibold text-main-color-obio-chedron dark:text-main-color-obio-amarillo">-Cuidado de la Piel</h3>
+        <h3 class="my-6 text-2xl font-semibold text-main-color-obio-chedron dark:text-main-color-obio-amarillo"><a href="/catalogo/Cuidado de la Piel">Cuidado de la Piel</a></h3>
         <ul>
-            <li>Crema corporal</li>
-            <li>Crema Facial</li>
-            <li>Shampoo</li>
+            <li><a href="/catalogo/Crema corporal">Crema corporal</a></li>
+            <li><a href="/catalogo/Crema Facial">Crema Facial</a></li>
+            <li><a href="/catalogo/Shampoo">Shampoo</a></li>
             
         </ul>
     </li>
     <li>
-        <h3 class="my-6 text-2xl font-semibold text-main-color-obio-chedron dark:text-main-color-obio-amarillo">-Herbolaria</h3>
+        <h3 class="my-6 text-2xl font-semibold text-main-color-obio-chedron dark:text-main-color-obio-amarillo"><a href="/catalogo/Herbolaria">Herbolaria</a></h3>
         <ul>
             <li></li>
             <li></li>
